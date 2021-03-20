baabc9370bc82db128f5a5f0f71c2e966655cc94
maurice1er <armeldrey@gmail.com>
2021-03-20 11:11:33 +0000

    Update App.js
    add text

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
           Hello World
+    
+          <p>This is the react app deploy with Heroku</p>
       </header>
     </div>
   );
